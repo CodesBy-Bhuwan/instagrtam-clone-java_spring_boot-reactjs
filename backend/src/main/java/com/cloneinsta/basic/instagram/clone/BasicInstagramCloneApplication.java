f0af2d6ce4d6d9614a59aad4103533d88beed669
CodesBy-Bhuwan <bhuwan.thadarai817@gmail.com>
2024-05-03 11:15:49 +0545

    setup layout initializaiton

---
+++ backend/src/main/java/com/cloneinsta/basic/instagram/clone/BasicInstagramCloneApplication.java
@@ -0,0 +1,13 @@
+package com.cloneinsta.basic.instagram.clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicInstagramCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicInstagramCloneApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cloneinsta/basic/instagram/clone/BasicInstagramCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloneinsta.basic.instagram.clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicInstagramCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
