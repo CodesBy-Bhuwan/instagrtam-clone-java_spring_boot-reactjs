ab292a4d7cae9fdb25935c4864854e776b4b3350
CodesBy-Bhuwan <bhuwan.thadarai817@gamil.com>
2024-05-04 00:04:26 +0545

    Debug

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 
+import React from "react";
 // import { Router } from 'react-router-dom';
 import Router from "./Pages/Router/Router";
 
 function App() {
   return (
     <div className="App">
-      <Router/>;
+      <Router/>
     </div>
   );
 }
