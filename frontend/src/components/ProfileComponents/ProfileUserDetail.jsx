c0a39c6957ed0ef5918121624496d53fa89a364e
CodesBy-Bhuwan <bhuwan.thadarai817@gamil.com>
2024-05-05 23:20:06 +0545

    UserProfilePicSurface

--- frontend/src/components/ProfileComponents/ProfileUserDetail.jsx
+++ frontend/src/components/ProfileComponents/ProfileUserDetail.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const ProfileUserDetail = () => {
   return (
-    <div>ProfileUserDetail</div>
+    <div className='py-10'>
+      <div>
+        <div>
+          <img className='w-32 h-32 rounded-full' src="" alt="" />
+        </div>
+      </div>
+    </div>
   )
 }
 
