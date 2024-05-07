3eda1f82ac8189a18277359201b75f086ef61f30
CodesBy-Bhuwan <bhuwan.thadarai817@gamil.com>
2024-05-07 21:15:21 +0545

    ProfilePostPage

--- frontend/src/components/ProfileComponents/ReqUserPostPart.jsx
+++ frontend/src/components/ProfileComponents/ReqUserPostPart.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {AiOutlineTable, AiOutlineUser} from 'react-icons/ai'
 import {RiVideoAddLine} from 'react-icons/ri'
 import {BiBookmark} from 'react-icons/bi'
+import ReqUserPostImg from './ReqUserPostImg'
+
 
 
 const ReqUserPostPart = () => {
+    const [ activeTab, setActiveTab] = useState();
 
     const tabs = [
         {
@@ -33,11 +36,16 @@ const ReqUserPostPart = () => {
 <div className='flex space-x-14 border-t relative'>
     {tabs.map((item)=>
 
-    <div className='flex items-center cursor-pointer py-2 text-sm'>
+    <div onClick={()=> setActiveTab(item.tab)} className={`${activeTab===item.tab?"border-t border-black":"opacity-60"} flex items-center cursor-pointer py-2 text-sm`}>
         <p>{item.icon}</p>
         <p className='ml-1'>{item.tab}</p>
     </div>
     )}
+</div>
+<div>
+    <div className='flex flex-wrap'>
+        {[1,1,1,1,1].map((item)=><ReqUserPostImg/>)}
+    </div>
 </div>
     </div>
   )
