64b543a739dbe567498fb32fda3053099f4df0e1
CodesBy-Bhuwan <bhuwan.thadarai817@gamil.com>
2024-05-07 22:04:42 +0545

    stories-section

--- frontend/src/Pages/HomePage/HomePage.jsx
+++ frontend/src/Pages/HomePage/HomePage.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import StoriesCircle from '../../components/StoriesComponents/StoriesCircle'
+import HomeRight from '../../components/HomeRight/HomeRight'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div>
+      <div className='mt-10 flex w-[100%] justify-center'>
+      <div className='w-[44%] px-10'>
+        <div className='storyDiv flex space-x-2 border p-4 rounded-md justify-start w-full'>
+          {[1,2,3].map((item)=> (<StoriesCircle/>))} 
+          </div>
+          <div>
+            post
+            {/* post */}
+          </div>
+      </div>
+      <div>
+        {/* Otherinfo in the Right hand side */}
+        homeiinfo
+        <HomeRight />
+      </div>
+      </div>
+    </div>
   )
 }
 
