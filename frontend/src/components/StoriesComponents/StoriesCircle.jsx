ff446a874f6d5ba0ce7b12e489535f07324307dc
CodesBy-Bhuwan <bhuwan.thadarai817@gamil.com>
2024-05-12 20:18:47 +0545

    Progressbar

--- frontend/src/components/StoriesComponents/StoriesCircle.jsx
+++ frontend/src/components/StoriesComponents/StoriesCircle.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const StoriesCircle = () => {
+  const navigate=useNavigate()
+  const handleNavigate=()=>{
+
+    navigate("/story")
+
+  }
   return (
-    <div className='cursor-pointer flex flex-col items-center'>
+    <div onClick={handleNavigate} className='cursor-pointer flex flex-col items-center'>
         <img className='w-16 h-16 rounded-full' 
         src="https://images.pexels.com/photos/135940/pexels-photo-135940.jpeg?auto=compress&cs=tinysrgb&w=600" 
         alt="" />
