5a205c6303c32ff2d9a5820f2ca1788f1b6cc142
CodesBy-Bhuwan <bhuwan.thadarai817@gamil.com>
2024-05-17 23:10:29 +0545

    AppConfig

--- backend/src/main/java/com/cloneinsta/basic/instagram/clone/config/AppConfig.java
+++ backend/src/main/java/com/cloneinsta/basic/instagram/clone/config/AppConfig.java
@@ -1,2 +1,19 @@
-package com.cloneinsta.basic.instagram.clone.config;public class AppConfig {
+package com.cloneinsta.basic.instagram.clone.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class AppConfig {
+
+    public SecurityFilterChain securityConfigration(HttpSecurity http) throws Exception{
+
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+        return http.build()
+
+    }
 }
